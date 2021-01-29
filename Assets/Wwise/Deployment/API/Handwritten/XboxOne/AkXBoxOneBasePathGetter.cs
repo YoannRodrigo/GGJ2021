e17a0935b4ab79106d24ab847363e239b0ba58d8
Yuwee <44063112+Yuwee@users.noreply.github.com>
2021-01-29 13:59:36 +0100

    Wwise integration
    YOLO

---
+++ Assets/Wwise/Deployment/API/Handwritten/XboxOne/AkXBoxOneBasePathGetter.cs
@@ -0,0 +1,6 @@
+﻿#if UNITY_XBOXONE && !UNITY_EDITOR
+public partial class AkBasePathGetter
+{
+	static string DefaultPlatformName = "XboxOne";
+}
+#endif
---
+++ Assets/Wwise/Editor/EditorPlatformHelpers/GX/AkGXPluginActivator.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR
+[UnityEditor.InitializeOnLoad]
+public class AkGXPluginActivator
+{
+	static AkGXPluginActivator()
+	{
+		AkPluginActivator.BuildTargetToPlatformName.Add((UnityEditor.BuildTarget)43 /* GameCoreXboxOne */, "GX");
+		AkBuildPreprocessor.BuildTargetToPlatformName.Add((UnityEditor.BuildTarget)43 /* GameCoreXboxOne */, "XboxOne");
+	}
+}
+#endif
