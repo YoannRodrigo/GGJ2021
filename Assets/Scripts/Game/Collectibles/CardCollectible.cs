3f2cd0285544f6a5488726e67a29211c214f95ec
Yoann <yomaisonneuve@gmail.com>
2021-01-30 17:25:32 +0100

    UpdateCard Received

--- Assets/Scripts/Game/Collectibles/CardCollectible.cs
+++ Assets/Scripts/Game/Collectibles/CardCollectible.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CardCollectible : MonoBehaviour
 {
@@ -10,17 +8,24 @@ public class CardCollectible : MonoBehaviour
     public Card.CardMajorType type;
     public Card.CardFamily family;
     public int value;
+    private bool isTaken;
 
     private void OnTriggerEnter(Collider other) {
-        switch(card.arcana){
-            case Card.CardArcana.MAJOR : 
-                MasterManager.Instance.cardsManager.AddCard(new MajorArcanaCard(cardName, card.arcana, type, card.sprite, card.description));
-            break;
-            case Card.CardArcana.MINOR : 
-                MasterManager.Instance.cardsManager.AddCard(new MinorArcanaCard(cardName, card.arcana, family, value, card.sprite, card.description));
-            break;
+        if(!isTaken)
+        {
+            isTaken = true;
+            switch (card.arcana)
+            {
+                case Card.CardArcana.MAJOR:
+                    MasterManager.Instance.cardsManager.AddCard(new MajorArcanaCard(cardName, card.arcana, type, card.sprite, card.description));
+                    break;
+                case Card.CardArcana.MINOR:
+                    MasterManager.Instance.cardsManager.AddCard(new MinorArcanaCard(cardName, card.arcana, family, value, card.sprite, card.description));
+                    break;
+            }
         }
 
+        print((gameObject.GetInstanceID()));
         Destroy(gameObject);
         
     }
