63016d3fef4c4bfc271851ccdb4bade9908e3b99
Yoann <yomaisonneuve@gmail.com>
2021-01-31 00:17:27 +0100

    Update Player, Update tuto1 tuto2

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using DG.Tweening;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,10 +13,15 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private List<GroundTile> path;
     [SerializeField] private Wisp wisp;
     private Rigidbody thisRigidbody;
+    [SerializeField] private Animator animator;
     //Cards
     [Space]
     [Header("Cards")]
     public List<Card> playerCards = new List<Card>();
+
+    private float speed;
+
+    private static readonly int SPEED = Animator.StringToHash("Speed");
 
 
     public void SetTarget(GroundTile target)
@@ -46,6 +52,7 @@ public class PlayerManager : MonoBehaviour
     {
         ActivateSwitchOnTile();
         MovePlayer();
+        animator.SetFloat(SPEED,speed);
     }
 
     private void MovePlayer()
@@ -61,12 +68,20 @@ public class PlayerManager : MonoBehaviour
                     path.RemoveAt(path.Count - 1);
                 }
 
+                transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+                speed = Mathf.Lerp(speed, 1, 5f * Time.deltaTime);
+                print(speed);
                 transform.DOMove(target.transform.position, 0.2f).SetEase(Ease.Linear);
             }
             else
             {
+                
                 path.Clear();
             }
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, 0, 5f * Time.deltaTime);
         }
     }
 
@@ -82,7 +97,15 @@ public class PlayerManager : MonoBehaviour
 
     public void Win()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        int buildIndex = SceneManager.sceneCountInBuildSettings;
+        if(SceneManager.GetActiveScene().buildIndex + 1 < buildIndex)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        { 
+            Application.Quit();
+        }
     }
 
     public Wisp GetWisp()
