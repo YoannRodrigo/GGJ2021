2b80ae8c8213ad3a5023424c0b24db2d3aae0f95
Cyril <cyrilgajan@gmail.com>
2021-01-30 02:51:29 +0100

    Remove useless using.

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using mechanism;
-using System;
 
 [RequireComponent(typeof(Renderer))]
 [RequireComponent(typeof(ParticleSystem))]
