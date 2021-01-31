c38401d6a42a34920d885fd5933b7c882de930d8
Yoann <yomaisonneuve@gmail.com>
2021-01-31 16:59:13 +0100

    Add SpalshScreen

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class SplashScreen : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer;
+    private bool isVideoOver;
+    private void Update()
+    {
+        if (!videoPlayer.isPlaying && !isVideoOver)
+        {
+            isVideoOver = true;
+            SceneManager.LoadScene(1);
+        }
+    }
+}
