eaaf63c6718c8566aa9caf30ac07e4402476d18a
Yoann <yomaisonneuve@gmail.com>
2021-01-31 17:14:43 +0100

    Update SplashScreen

--- Assets/SplashScreen.cs
+++ Assets/SplashScreen.cs
@@ -5,12 +5,18 @@ using UnityEngine.Video;
 public class SplashScreen : MonoBehaviour
 {
     [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private Canvas canvas;
     private bool isVideoOver;
     private void Update()
     {
         if (!videoPlayer.isPlaying && !isVideoOver)
         {
             isVideoOver = true;
+            canvas.gameObject.SetActive(true);
+        }
+
+        if (isVideoOver && Input.GetMouseButtonDown(0))
+        {
             SceneManager.LoadScene(1);
         }
     }
