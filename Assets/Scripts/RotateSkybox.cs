698992801a205138d93dde2561eae16a94c34ca3
Yoann <yomaisonneuve@gmail.com>
2021-01-31 01:08:14 +0100

    Add better SkyBox

---
+++ Assets/Scripts/RotateSkybox.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    
+    private static readonly int ROTATION = Shader.PropertyToID("_Rotation");
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update ()
+    {
+        RenderSettings.skybox.SetFloat(ROTATION, Time.time);
+        //To set the speed, just multiply Time.time with whatever amount you want.
+    }
+}
